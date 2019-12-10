46ab6e1833abbb9e1372b8346009c79e7c736312
Shreya Pallan <shreya.pallan@gmail.com>
2019-12-10 10:19:22 +0530

    Initial Commit

---
+++ GoodNews/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GoodNews
+//
+//  Created by Shreya on 10/12/19.
+//  Copyright © 2019 Shreya Pallan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
